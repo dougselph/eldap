11ecc9d3bdba731edd800ea0af8b077ad0e89db6
Doug Selph <dselph@gmail.com>
2012-02-02 18:05:00 -0600

    Ensure that public_key and ssl are started at application start.

--- src/eldap_app.erl
+++ src/eldap_app.erl
@@ -9,8 +9,18 @@
 %% Application callbacks
 %% ===================================================================
 
+ensure_started(App) ->
+	case application:start(App) of
+		ok ->
+			ok;
+		{error, {already_started, App}} ->
+			ok
+	end.
+
 start(_StartType, _StartArgs) ->
+		[ensure_started(A) || A <- [crypto, public_key, ssl]],
     eldap_sup:start_link().
 
 stop(_State) ->
+		[application:stop(A) || A <- [ssl, public_key, crypto]],
     ok.
